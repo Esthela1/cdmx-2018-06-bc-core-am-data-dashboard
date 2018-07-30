ea25f1f86bbc612381f94161f290d0e6b40ae16a
Esthela1 <esthela_salas1212@hotmail.com>
2018-07-30 17:04:48 -0500

    inicio de acomodo de la informacion lima tercera generacion

--- src/data.js
+++ src/data.js
@@ -1,25 +1,25 @@
-    window.dashboard = {
-        computeStudentsStats: (laboratoria) => {
-            laboratoria = 'https://api.myjson.com/bins/13lpdy';
-            fetch(laboratoria).then((data) => {
-                    return data.json();
-                })
-                .then((data) => {
-                    laboratoriaData(data);
-                }).catch((err) => {
-                    console.log('Error en laboratoriaData');
-                })
-        },
-        computeGenerationStats: (laboratoria) => {
-            laboratoria = 'https://api.myjson.com/bins/13lpdy';
-            fetch(laboratoria).then((data) => {
-                    return data.json();
-                })
-                .then((data) => {
-                    generationData(data);
-                })
-                .catch((err) => {
-                    console.log('Error en generationData');
-                })
-        }
-    }
+window.dashboard = {
+  computeStudentsStats: (laboratoria) => {
+    laboratoria = 'https://api.myjson.com/bins/13lpdy';
+    fetch(laboratoria).then((data) => {
+      return data.json();
+    })
+      .then((data) => {
+        laboratoriaData(data);
+      }).catch((err) => {
+        console.log('Error en laboratoriaData');
+      })
+  },
+  computeGenerationStats: (laboratoria) => {
+    laboratoria = 'https://api.myjson.com/bins/13lpdy';
+    fetch(laboratoria).then((data) => {
+      return data.json();
+    })
+      .then((data) => {
+        generationData(data);
+      })
+      .catch((err) => {
+        console.log('Error en generationData');
+      })
+  }
+}
